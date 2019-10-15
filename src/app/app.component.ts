6f525718441405fabc97cb8eab74503473ef40bd
prayasdrolia1 <prayasdrolia1@users.noreply.github.com>
2019-10-15 15:38:14 +0530

    Heroku Deployment

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.table_page.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myapp1';
+}
